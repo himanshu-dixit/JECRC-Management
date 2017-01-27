b95bd2b461cbb1d99be9b0874a263e33c748c5d8
Himanshu Dixit <hudixt@gmail.com>
2017-01-27 12:48:00 +0530

    Sidebar Change

--- components/sidebar.php
+++ components/sidebar.php
@@ -45,20 +45,56 @@
 
 			<ul id="main-menu" class="main-menu">
 
-
-				<li>
+				<!-- Only For Administrator-->
+					<li>
+					<a href="manage.php">
+						<i class="entypo-layout"></i>
+						<span class="title">Manage</span>
+					</a>
+				</li>
+					<li>
+						<a href="add.php">
+						<i class="entypo-window"></i>
+						<span class="title">Add Attendance</span>
+						</a>
+					</li>
+					<li>
+						<a href="reports.php">
+						<i class="entypo-chart-bar"></i>
+						<span class="title">Reports</span>
+						</a>
+					</li>
+					<li>
 					<a href="schedule.php">
 						<i class="entypo-flow-tree"></i>
 						<span class="title">Schedule</span>
 					</a>
 				</li>
-
+				<li>
+					<a href="notifications.php">
+						<i class="entypo-doc-text"></i>
+						<span class="title">Notifications</span>
+					</a>
+				</li>
+				<li>
+					<a href="mail.php">
+					<i class="entypo-mail"></i>
+					<span class="title">Send Mail</span>
+					</a>
+				</li>
+				<li>
+					<a href="assignments.php">
+					<i class="entypo-bag"></i>
+					<span class="title">Assignments</span>
+					</a>
+				</li>
 				<li>
 					<a href="contact.php">
-						<i class="entypo-mail"></i>
+						<i class="entypo-newspaper"></i>
 						<span class="title">Contact</span>
 					</a>
     	</li>
+
 
 			</ul>
 
