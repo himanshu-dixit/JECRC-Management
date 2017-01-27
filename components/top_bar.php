9e17d035c98c97f4f4262cb1ef6cbe678fcb8d33
Himanshu Dixit <hudixt@gmail.com>
2017-01-27 10:12:18 +0530

    Top Bar

---
+++ components/top_bar.php
@@ -0,0 +1,22 @@
+<div class="row">
+
+
+	<!-- Raw Links -->
+	<div class="col-md-12 col-sm-4 clearfix hidden-xs">
+
+		<ul class="list-inline links-list pull-right">
+
+			<li class="sep"></li>
+
+			<li>
+				<a href="extra-login.html">
+					Log Out <i class="entypo-logout right"></i>
+				</a>
+			</li>
+		</ul>
+
+	</div>
+
+</div>
+
+<hr />
