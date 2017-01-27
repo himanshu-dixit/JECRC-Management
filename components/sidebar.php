c995a365db04668fc6860811aef8e79373432be6
Himanshu Dixit <hudixt@gmail.com>
2017-01-27 10:11:46 +0530

    Sidebar.php

---
+++ components/sidebar.php
@@ -0,0 +1,67 @@
+
+	<div class="sidebar-menu">
+
+		<div class="sidebar-menu-inner">
+
+			<header class="logo-env">
+
+				<!-- logo -->
+				<div class="logo">
+					<a href="index.html">
+						<img src="http://www.jecrcudml.edu.in/images/jecrc-foundation.png" width="140" alt="" />
+					</a>
+				</div>
+
+				<!-- logo collapse icon -->
+				<div class="sidebar-collapse">
+					<a href="#" class="sidebar-collapse-icon"><!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
+						<i class="entypo-menu"></i>
+					</a>
+				</div>
+
+
+				<!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
+				<div class="sidebar-mobile-menu visible-xs">
+					<a href="#" class="with-animation"><!-- add class "with-animation" to support animation -->
+						<i class="entypo-menu"></i>
+					</a>
+				</div>
+
+			</header>
+
+						<div class="sidebar-user-info">
+
+				<div class="sui-normal">
+					<a href="#" class="user-link">
+						<img src="assets/images/thumb-1.png" alt="" class="img-circle" />
+
+						<span>Welcome,</span>
+						<strong>Art Ramadani</strong>
+					</a>
+				</div>
+
+			</div>
+
+
+			<ul id="main-menu" class="main-menu">
+
+
+				<li>
+					<a href="schedule.php">
+						<i class="entypo-flow-tree"></i>
+						<span class="title">Schedule</span>
+					</a>
+				</li>
+
+				<li>
+					<a href="contact.php">
+						<i class="entypo-mail"></i>
+						<span class="title">Contact</span>
+					</a>
+    	</li>
+
+			</ul>
+
+		</div>
+
+	</div>
